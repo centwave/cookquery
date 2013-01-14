7c6d9dd8c1b1e653f9bb0b06e074afca6b658c0f
centwave <centwave@gmail.com>
2013-01-14 23:32:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookquery::Application.config.secret_token = '47a4b8bf09085fb233c0c514251109b9e4cc7f47f2cd9dfe100bf71b67ccddc8e24de7c41a97d1a7aea9800075f6f0f03762fc8c95a0b4cea21b9aaa74520ef7'
